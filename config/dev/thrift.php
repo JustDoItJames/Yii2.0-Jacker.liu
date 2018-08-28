3b670f6182929b5c1c477a3f36131ba3403e62d4
JustDoItJames <31331012+JustDoItJames@users.noreply.github.com>
2018-08-28 20:14:14 +0800

    Update thrift.php

--- config/dev/thrift.php
+++ config/dev/thrift.php
@@ -3,12 +3,11 @@ return [
     'thrift'=>array(
         'class'=>'app\components\thrift\ThriftClientOrigin',
         'serviceConfig'=>array(
-            /* 用户中心相关服务 */
             'ThriftUserInfoService'=>array(
-                'dirName'           => 'userinfocenter',
-                'className'         => 'ThriftUserInfoService',
-                'serverHost'        => '10.1.2.191',
-                'serverPort'        => '50599',
+                'dirName'           => 'userinfocenter111',
+                'className'         => 'ThriftUserInfoService111',
+                'serverHost'        => '',
+                'serverPort'        => '',
                 'sendTimeout'       => 15,
                 'recvTimeout'       => 15,
                 'maxConnectTimes'   => 2,
