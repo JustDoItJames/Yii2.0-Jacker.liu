8e5ab1a3d44578559f8185c6d952a5c08e558ef5
JustDoItJames <31331012+JustDoItJames@users.noreply.github.com>
2018-08-28 19:52:04 +0800

    Update alioss.php

--- config/alioss.php
+++ config/alioss.php
@@ -1,9 +1,4 @@
 <?php
 return [
     'class' => 'app\components\oss\OSS',
-//    'accessKeyId' => 'LTAIyHyBTyQZbGIK', // 阿里云OSS AccessKeyID
-//    'accessKeySecret' => 'zBbNTrU3rtLaGnn37FvGtVb6y1FS3d', // 阿里云OSS AccessKeySecret
-//    'bucket' => 'jfz-static2', // 阿里云的bucket空间
-//    'lanDomain' => 'oss-cn-hangzhou-internal.aliyuncs.com', // OSS内网地址
-//    'wanDomain' => 'oss-cn-hangzhou.aliyuncs.com', //OSS外网地址
 ];
